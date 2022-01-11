01af1ca38b4e12cad7b241b25fe14e8495a7b93d
gastonmontenegro <97260638+gastonmontenegro@users.noreply.github.com>
2022-01-11 19:36:10 -0300

    Ejercicio Mercado

--- src/index.ts
+++ src/index.ts
@@ -7,32 +7,38 @@ let nombreSupermercado:string = "El Camino";
   let producto1:string = "Azucar";
     let producto2:string = "Te";
       let producto3:string = "Aerosol";
-    let producto4:string = "Harina";
-  let producto5:string = "Leche";
-let producto6:string = "Yogurt";
+        let producto4:string = "Harina";
+          let producto5:string = "Leche";
+            let producto6:string = "Yogurt";
 
 //Precio articulos
 let precioArticulo1:number = 10;
   let precioArticulo2:number = 25;
     let precioArticulo3:number = 90;
       let precioArticulo4:number = 55;
-  let precioArticulo5:number = 18;
-let precioArticulo6:number = 71;
+        let precioArticulo5:number = 18;
+          let precioArticulo6:number = 71;
 
 //Stock
 let stock1:number = 100;
   let stock2:number = 40;
     let stock3:number = 55;
       let stock4:number = 84;
-    let stock5:number = 61;
-let stock6:number = 18;
+        let stock5:number = 61;
+          let stock6:number = 18;
 
+//Imprime en consola Productos, precio y stock
 console.log("Mercado:", nombreSupermercado);
   console.log("Producto:"producto1, "Precio $"precioArticulo1, "unidades:"stock1);
     console.log("Producto:"producto2, "Precio $"precioArticulo2, "unidades:"stock2);
       console.log("Producto:"producto3, "Precio $"precioArticulo3, "unidades:"stock3);
-    console.log("Producto:"producto4, "Precio $"precioArticulo4, "unidades:"stock4);
-  console.log("Producto:"producto5, "Precio $"precioArticulo5, "unidades:"stock5);
-console.log("Producto:"producto6, "Precio $"precioArticulo6, "unidades:"stock6);
+        console.log("Producto:"producto4, "Precio $"precioArticulo4, "unidades:"stock4);
+          console.log("Producto:"producto5, "Precio $"precioArticulo5, "unidades:"stock5);
+            console.log("Producto:"producto6, "Precio $"precioArticulo6, "unidades:"stock6);
 
-//Soy nuevo y éste es mi primer ejercicio.
+//Compra de producto
+console.log("COMPRA")
+  console.log("Articulo:"producto6,"-- Precio $",precioArticulo6, "-- Stock Restante:"stock6 - 1, "unid.");
+    console.log("Articulo:"producto6,"-- Precio $",precioArticulo6, "-- Stock Restante:"stock6 - 2,"unid.");
+      console.log("Articulo:"producto4,"-- Precio $",precioArticulo4, "-- Stock Restante:"stock4 - 1,"unid.");
+        console.log("Total $", precioArticulo6 * 2 + precioArticulo4);
