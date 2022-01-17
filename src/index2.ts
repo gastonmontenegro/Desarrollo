66fd252cc82e220ae80f69ba06ec704b81cf2f51
gastonmontenegro <97260638+gastonmontenegro@users.noreply.github.com>
2022-01-17 18:52:30 -0300

    Ejercicio Mercado

---
+++ src/index2.ts
@@ -0,0 +1,6 @@
+// Ejercicio Descuento
+const producto:number = 450.50;
+let porcentajeDescuento:number = 0.1;
+let descuento = document.getElementById("dato");
+
+console.log(descuento = producto * porcentajeDescuento);
