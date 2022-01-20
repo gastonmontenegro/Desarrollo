6ea409d17cf38c30fdd803fbc2bb5d83d2a38f63
gastonmontenegro <97260638+gastonmontenegro@users.noreply.github.com>
2022-01-20 19:15:47 -0300

    Mercado Entrega 3

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,44 @@
-//Ejercicio Mercado
+let producto1 = prompt("ingresar el nombre del producto");
+let producto2 = prompt("ingresar el nombre del segundo producto");
+let producto3 = prompt("ingresar el nombre del tercer producto");
+let stock1: number = Number(
+  prompt("ingresar stock disponible del primer producto")
+);
+let stock2: number = Number(
+  prompt("ingresar stock disponible del segundo producto")
+);
+let stock3: number = Number(
+  prompt("ingresar stock disponible del tercer producto")
+);
+let precio1: number = Number(prompt("ingresar precio del primer producto"));
+let precio2: number = Number(prompt("ingresar precio del segundo producto"));
+let precio3: number = Number(prompt("ingresar precio del tercer producto"));
 
-//Nombre Comercio
-let nombreSupermercado:string = "El Camino";
+console.log("Bienvenido al Supermercado");
+console.log(
+  `Nuestros productos disponibles son  ${stock1} ${producto1}, cuesta ${precio1} cada uno, ${stock2} ${producto2}, cuesta ${precio2} cada uno, ${stock3} ${producto3}, cuesta ${precio3} cada uno.`
+);
+let compra1: number = Number(prompt(`cuantas ${producto1} desea llevar?`));
+let compra2: number = Number(prompt(`cuantas ${producto2} desea llevar?`));
+let compra3: number = Number(prompt(`cuantas ${producto3} desea llevar?`));
 
-//Articulos
-let producto1:string = "Azucar";
-let producto2:string = "Te";
-let producto3:string = "Aerosol";
-let producto4:string = "Harina";
-let producto5:string = "Leche";
-let producto6:string = "Yogurt";
+let calculo1: number = precio1 * compra1;
+let calculo2: number = precio2 * compra2;
+let calculo3: number = precio3 * compra3;
+let precioTotal: number = calculo1 + calculo2 + calculo3;
+let stockRestante1: number = stock1 - compra1;
+let stockRestante2: number = stock2 - compra2;
+let stockRestante3: number = stock3 - compra3;
 
-//Precio articulos
-let precioArticulo1:number = 10.20;
-let precioArticulo2:number = 25.50;
-let precioArticulo3:number = 90.01;
-let precioArticulo4:number = 55.71;
-let precioArticulo5:number = 18.30;
-let precioArticulo6:number = 71.05;
-
-//Stock
-let stock1:number = 100;
-let stock2:number = 40;
-let stock3:number = 55;
-let stock4:number = 84;
-let stock5:number = 61;
-let stock6:number = 18;
-
-//Imprime en consola Productos, precio y stock
-console.log("Mercado:", nombreSupermercado);
-console.log("Producto:"producto1, "Precio $"precioArticulo1, "unidades:"stock1);
-console.log("Producto:"producto2, "Precio $"precioArticulo2, "unidades:"stock2);
-console.log("Producto:"producto3, "Precio $"precioArticulo3, "unidades:"stock3);
-console.log("Producto:"producto4, "Precio $"precioArticulo4, "unidades:"stock4);
-console.log("Producto:"producto5, "Precio $"precioArticulo5, "unidades:"stock5);
-console.log("Producto:"producto6, "Precio $"precioArticulo6, "unidades:"stock6);
-
-//Compra de producto
-console.log("=== COMPRA ===")
-console.log("Articulo 1:"producto6,"$",precioArticulo6);
-console.log("Articulo 2:"producto6,"$",precioArticulo6);
-console.log("Articulo 3:"producto4,"$",precioArticulo4,);
-console.log("Total Compra $", precioArticulo6 * 2 + precioArticulo4);
-console.log("==============");
-console.log("Stock Restante", producto6,":", stock6 - 2,"unidades");
-console.log("Stock Restante", producto4,":", stock4 - 1,"unidades");
+if (compra1 > stock1 && compra2 > stock2 && compra3 > stock3) {
+  console.log(`no tenemos stock suficiente para su pedido, intente de vuelta`);
+} else {
+  console.log(`has comprado ${compra1} ${producto1}, lo que seria ${calculo1}`);
+  console.log(`has comprado ${compra2} ${producto2}, lo que seria ${calculo2}`);
+  console.log(`has comprado ${compra3} ${producto3}, lo que seria ${calculo3}`);
+  console.log(`el precio total seria ${precioTotal}`);
+  console.log(
+    `el stock restante seria ${stockRestante1} ${producto1} ${stockRestante2} ${producto2} ${stockRestante3} ${producto3}`
+  );
+  console.log("muchas gracias por su compra, vuelva pronto!");
+}
--- src/index2.ts
+++
@@ -1,6 +0,0 @@
-// Ejercicio Descuento
-const producto:number = 450.50;
-let porcentajeDescuento:number = 0.1;
-let descuento = document.getElementById("dato");
-
-console.log(descuento = producto * porcentajeDescuento);
